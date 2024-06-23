206db346d1fd5083ccc6100dfc7639a7088a8d8a
Fox1508 <fvantonder00@gmail.com>
2024-06-23 18:05:00 +0200

    NEW METHOD: Pipes fixed
    The old method of drawing 2 pipes was scrapped because I couldnt figure it out. But that aside, the new method draws one full rect = to the screen height, and then another rect ,with the same width but the height of the PYPE_GAP, drawn optop of it. The starting y-point of the gap rect is calculated and then drawn to the specifications provided.

--- Demo/pypes.py
+++ Demo/pypes.py
@@ -4,57 +4,36 @@ from setting import *
 class Pype:
     def __init__(self):
         # Pypes
-        self.bottomPipes = []
-        self.topPipes = []
-
-    def initPypes(self): # inits the  first pypes on the game starting
-        H = random.randint(PYPE_GAP,HEIGHT)
-        self.topPipes.append({
-            'x' : PYPE_X,
-            'y' : 0,
-            'height' : H
-        })
-        self.bottomPipes.append({
-            'x' : PYPE_X,
-            'y' : self.topPipes[0]['height'] + PYPE_GAP,
-            'height' :  HEIGHT - self.topPipes[0]['height'] + PYPE_GAP
-        })
+        self.pypes = []
 
     def get_pypes(self,window):
-        # Add initial objects
-        if len(self.topPipes) == 0 and len(self.bottomPipes) == 0:
-            self.initPypes()
+        if len(self.pypes) == 0:
+            self.init_pypes()
 
-        # for amount of pypes in top pypes - which is the same in the bottom pypes list
-        # this should let the pair of pypes be drawn at the same time and not the all the top/bottom pypes first or second
-        for num in range(len(self.topPipes)):
-            top = self.topPipes[num] # top part of pype pair
-            bottom = self.bottomPipes[num] # bottom part of pype pair
-
-            if top['x'] == WIDTH - PYPE_W*4: # Adding a new pype obj to list of pype parts when the leading pype reaches point 'x'
-                H = random.randint(PYPE_GAP,HEIGHT)
-                self.topPipes.append({
+        for pype in self.pypes:
+            if pype['x'] == PYPE_SPAWN:
+                gap_start_Y = random.randint(0,HEIGHT-PYPE_GAP)
+                self.pypes.append({
                     'x' : PYPE_X,
-                    'y' : 0,
-                    'height' : H
+                    'gap' : gap_start_Y
                 })
-            if bottom['x'] == WIDTH - PYPE_W*4: # Adding a new pype obj to list of pype parts when the leading pype reaches point 'x'
-                self.bottomPipes.append({
-                    'x' : PYPE_X,
-                    'y' : self.topPipes[num]['height'] + PYPE_GAP,
-                    'height' : HEIGHT - self.topPipes[num]['height'] + PYPE_GAP
-                })
+            pype['x'] -= PYPE_VEL
+            self.draw_pypes_pairs(window,pype)
 
-            print(f'{num} TOP : \n > "Height" { top["height"] }\n > "y" { top["y"] }')
-            print(f'{num} Bottom : \n > "Height" { bottom["height"] }\n > "y" { bottom["y"] }')
+    def init_pypes(self):
+        gap_start_Y = random.randint(0,HEIGHT-PYPE_GAP)
+        self.pypes.append({
+            'x' : PYPE_X,
+            'gap' : gap_start_Y
+        })
 
-            # Update pype velocity
-            top['x'] -= PYPE_VEL
-            bottom['x'] -= PYPE_VEL
 
-            # Drawing pypes
-            self.draw_pypes_pairs(window,top,bottom)
 
-    def draw_pypes_pairs(self,window,top,bottom):
-        pygame.draw.rect(window, GREEN, (top['x'], top['y'] , PYPE_W , top['height']))
-        pygame.draw.rect(window, GREEN, (bottom['x'], bottom['y'] , PYPE_W , bottom['height']))
+
+
+
+    def draw_pypes_pairs(self,window,pype):
+        pygame.draw.rect(window,GREEN,(pype['x'], 0, PYPE_W, HEIGHT)) # drawing pype
+        pygame.draw.rect(window,RED, (pype['x'], pype['gap'], PYPE_W, PYPE_GAP)) # drawing gap
+        # pygame.draw.rect(window, GREEN, (top['x'], top['y'] , PYPE_W , HEIGHT))
+        # pygame.draw.rect(window, GREEN, (bottom['x'], bottom['y'] , PYPE_W , HEIGHT))
